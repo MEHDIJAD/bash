208dd12a0ea15a6d65aab9d878b994575bc5a67c
El Mehdi El Garouaz <eel-garo@e2r8p14.1337.ma>
2025-04-17 15:25:00 +0100

    add expand/env-(get the env variables in linked list)

---
+++ parser/expander/expander.c
@@ -0,0 +1,6 @@
+#include "../parser.h"
+
+// void	ft_expander(t_token *token, t_env *env)
+// {
+	
+// }
--- parser/parser.h
+++ parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: eel-garo <eel-garo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:16:43 by eel-garo          #+#    #+#             */
-/*   Updated: 2025/04/16 16:54:30 by eel-garo         ###   ########.fr       */
+/*   Updated: 2025/04/17 15:09:15 by eel-garo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <readline/history.h>
 # include <stdbool.h>
 # include <errno.h> //! MBLater
+# include <string.h> //^ for strcmp make your's
 
 typedef enum s_token_type
 {
@@ -37,6 +38,15 @@ typedef struct s_token
 	char			*value;
 	struct s_token	*next;
 }	t_token;
+
+typedef struct s_env
+{
+	char	*name;
+	char	*value;
+	int		flag_env;
+	struct s_env *next;
+}	t_env;
+
 
 //libft
 int		ft_isspace(int c);
@@ -64,5 +74,18 @@ void	ft_token_clear(t_token **token_list);
 int		ft_token_size(t_token *head); // might need later
 void	ft_print_token_list(t_token *head); // just for printing
 int		ft_synax_error_free(const char *line);
+
+// expander Part
+void	ft_expander(t_token *token, t_env *env);
+
+//	expander/env
+t_env	*ft_getenv(char **env);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+t_env	*ft_tenv_new(char *name, char *value, int flag);
+void	ft_tenv_add_back(t_env **env_list, t_env *new_node);
+t_env	*ft_tenv_last(t_env *env);
+void	ft_tenv_delone(t_env *node);
+void	ft_tenv_clear(t_env **token_list);
 
 #endif
